2bacd4562b61f2372f3e8a14e52647d6b5756417
Dark <dark.devil1055@gmail.com>
2025-08-24 14:58:23 +0530

    initial commit12

--- src/Backend/src/server.js
+++ src/Backend/src/server.js
@@ -30,10 +30,10 @@ app.use("/api/chat",chatRoute)
 app.use("/api/communities", communityRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../Frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../Frontend/dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../../frontend/dist", "index.html"));
   });
 }
 
